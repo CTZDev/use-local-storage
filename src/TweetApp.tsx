ac3a8e9789e9b396afbeb640f66f926afc10e44f
CTZDev <carlosteranzavaleta@hotmail.com>
2024-11-03 21:41:12 -0500

    (feat) Add localStorage with CR of Tweets

--- src/TweetApp.tsx
+++ src/TweetApp.tsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./TweetApp.css";
 import { AddTweet } from "./components/AddTweet";
 import { Header } from "./components/Header";
-import { ListTweets } from "./components/ListTweets";
+import ListTweets from "./components/ListTweets";
 import { TweetContainer } from "./components/TweetContainer";
+import type { Tweet } from "./types/Tweet";
 
 const TweetApp: React.FC = () => {
-  const [tweets, setTweets] = useState<Array<string>>([]);
+  const getTweetsOfLocalStorage = localStorage.getItem("tweets")
+    ? JSON.parse(localStorage.getItem("tweets") as string)
+    : [];
+
+  const [tweets, setTweets] = useState<Array<Tweet>>(getTweetsOfLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("tweets", JSON.stringify(tweets));
+  }, [tweets]);
 
   const handleNewTweet = (newTweet: string) => {
-    const tweet = newTweet.toUpperCase();
-    if (tweets.includes(tweet)) return alert("Ya existe el tweet, prueba con otro 😎");
-    setTweets((prev) => [...prev, tweet]);
+    const tweetData = {
+      id: crypto.randomUUID(),
+      name: newTweet.trim(),
+    };
+
+    if (tweets.some((tweet) => tweet.name === tweetData.name))
+      return alert("Ya existe el tweet, prueba con otro 😎");
+    setTweets((prev) => [...prev, tweetData]);
   };
 
   return (
